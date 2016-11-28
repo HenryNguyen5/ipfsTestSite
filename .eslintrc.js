135e6ff7a6a6dedb5db988fcd46367ebeb4eae80
omotherhen <ngu.henry@hotmail.com>
2016-11-28 18:28:28 -0500

    add config files

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+    "extends": [
+        "google"
+    ],
+    "plugins": [
+        "no-async-without-await"
+    ],
+
+    "parserOptions": {
+        "ecmaVersion": 8,
+    },
+    "rules": {
+        "no-async-without-await/no-async-without-await": 1,
+        "max-len": 0,
+    },
+
+};

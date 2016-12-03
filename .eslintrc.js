bb299eb4484c29820151d02744396fbbe3a3c5de
omotherhen <ngu.henry@hotmail.com>
2016-12-03 11:14:50 -0500

    electron ex

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,8 @@ module.exports = {
     "rules": {
         "no-async-without-await/no-async-without-await": 1,
         "max-len": 0,
+        "quotes": 0,
+        "semi": 0
     },
 
 };
--- src/app.js
+++ src/app.js
@@ -2,46 +2,49 @@ const OrbitDB = require('orbit-db');
 const IPFS = require('ipfs-daemon');
 
 const ipfsOptions = {
-    IpfsDataDir: '/tmp/' + 7,
+    IpfsDataDir: '/tmp/' + 8,
     Addresses: {
         API: '/ip4/127.0.0.1/tcp/0',
         Swarm: ['/ip4/0.0.0.0/tcp/0'],
-        Gateway: '/ip4/0.0.0.0/tcp/0'
+        Gateway: '/ip4/0.0.0.0/tcp/0',
     },
-}
+};
 
 const ipfs = new IPFS(ipfsOptions);
 ipfs.on('ready', () => {
     console.log("IPFS READY");
-    const orbitdb = new OrbitDB(ipfs, 7);
+
+    const orbitdb = new OrbitDB(ipfs, 8);
     const db = orbitdb.kvstore('application.settings.henry');
-    console.log("\n\n\n")
+    console.log("\n\n\n");
     console.log("PUTTING VALUE");
-  //  db.events.on('ready', () => {
+    db.events.on('ready', () => {
+        function go() {
 
-        console.log('\n\n\n DB READY');
+            let random = Math.random() * 100;
 
-        db.put('Hjhgfdfghjkjhgfdfghjk', 100)
-            .then(() => {
-              console.log("GETTING H1", db.get('Hjhgfdfghjkjhgfdfghjk'))
-            })
-            .catch((e) => {
-                console.log(e.stack)
-            });
-    //});
+            db.put('poo' + random, 'poop')
+                .then(() => {
+                    console.log("GETTING H1", db.get('poo' + random));
+                })
+                .catch((e) => {
+                    console.log(e.stack);
+                });
+        }
 
+        setInterval(go, 1000);
+    });
 
-
-    //db.events.on('data', (dbname, event) => console.log("Data", dbname, event))
-    //db.events.on('sync', () => console.log("Sync"))
-    //db.events.on('load', (dbname, hash) => console.log("Load", dbname, hash))
-    //db.events.on('history', (dbname, entries) => console.log(dbname, entries, "History"))
-    db.events.on('ready', (dbname) => console.log(dbname, "ready"))
-    db.events.on('write', (dbname, hash) => {console.log("write", dbname, hash)
-    console.log("GETTING H2", db.get('Hjhgfdfghjkjhgfdfghjk'))
-    })
+    db.events.on('data', (dbname, event) => console.log("Data", dbname, event));
+    db.events.on('sync', () => console.log("Sync"));
+    db.events.on('load', (dbname, hash) => console.log("Load", dbname, hash));
+    db.events.on('history', (dbname, entries) => console.log(dbname, entries, "History"));
+    db.events.on('ready', (dbname) => console.log(dbname, "ready"));
+    db.events.on('write', (dbname, hash) => {
+        console.log("write", dbname, hash);
+    });
 });
 
-ipfs.on('error', (e) => {
+ipfs.onc('error', (e) => {
     console.log("IPFS ERR" + e.stack);
 });
